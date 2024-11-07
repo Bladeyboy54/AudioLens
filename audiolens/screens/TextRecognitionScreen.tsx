98bb402ac5e7688a4da432a17592ef9e1e8f5dab
Bladeyboy54 <221146@virtualwindow.co.za>
2024-11-07 19:19:13 +0200

    Updated UI on Text Recognition page

--- audiolens/screens/TextRecognitionScreen.tsx
+++ audiolens/screens/TextRecognitionScreen.tsx
@@ -32,11 +32,16 @@ const TextRecognitionScreen = () => {
 
   return (
     <View style={styles.container}>
+
+      <Text style={styles.header}>Text Recognition</Text>
+
       <TextInput
         style={styles.input}
         placeholder="Enter text"
         value={text}
         onChangeText={setText}
+        multiline={true}
+        numberOfLines={4}
       />
       <TouchableOpacity
         style={[styles.button, loading && styles.buttonDisabled]}
@@ -45,7 +50,7 @@ const TextRecognitionScreen = () => {
       >
         <Text style={styles.buttonText}>{loading ? "Processing..." : "Convert to Speech"}</Text>
       </TouchableOpacity>
-      {loading && <ActivityIndicator size="small" color="#0000ff" />}
+      {loading && <ActivityIndicator size="large" color="rgba(255, 184, 84, 1)" />}
 
       <TouchableOpacity  
         onPress={() => navigation.navigate("Home")}
@@ -61,40 +66,49 @@ const TextRecognitionScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#1C1C1E',
+    // alignItems: 'center',
+    paddingHorizontal: 20,
     justifyContent: 'center',
+  },
+  header: {
+    fontSize: 28,
+    color: 'rgba(255, 184, 84, 1)',
+    fontWeight: '600',
+    textAlign: 'center',
+    marginBottom: 30,
   },
   input: {
     borderWidth: 1,
-    borderColor: "#f2f2f2",
-    padding: 10,
-    marginBottom: 20
+    borderColor: 'rgba(255, 184, 84, 1)',
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 20,
+    color: '#FFF',
+    fontSize: 16,
   },
   backButton: {
-    backgroundColor: '#007bff',
+    backgroundColor: 'rgba(60, 42, 106, 1)',
     paddingVertical: 12,
     paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 20,
-    width: '60%',
+    borderRadius: 8,
     alignItems: 'center',
+    marginTop: 20,
   },
   button: {
-    backgroundColor: '#007bff',
+    backgroundColor: 'rgba(60, 42, 106, 1)',
     paddingVertical: 12,
     paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 20,
-    width: '60%',
+    borderRadius: 8,
     alignItems: 'center',
+    marginVertical: 10,
   },
   buttonDisabled: {
     backgroundColor: '#a9a9a9',
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    color: 'rgba(255, 184, 84, 1)',
+    fontSize: 18,
     fontWeight: 'bold',
   },
 
